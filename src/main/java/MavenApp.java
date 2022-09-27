a576dc46f61c2e27a6c3d3059dc2acbec1d4e081
Danilo Korać <72507305+danilokorac@users.noreply.github.com>
2022-09-27 14:41:36 +0200

    Changes of folder

--- src/main/java/MavenApp.java
+++ src/main/java/MavenApp.java
@@ -4,7 +4,7 @@ public class MavenApp {
     public static void main(String[] args) {
 
         Employee emp = new Employee("Maven", 167, 1200);
-        
+
         double workerSalaryCount = emp.salaryCount();
 
         System.out.println("Worker with name " + emp.getName() + " has salary of " + workerSalaryCount + "RSD");
